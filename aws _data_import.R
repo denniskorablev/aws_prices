8b38894ddfe2db5f2d96408a54e59638488a9baa
Dennis Lyubyvy <dennis.lyubyvy@gmail.com>
2015-02-24 21:43:08 -0500

    ugly json data

--- aws _data_import.R
+++ aws _data_import.R
@@ -1,10 +1,14 @@
-library(jsonlite)
+# library(jsonlite)
+library(RJSONIO)
 
 # url with some information about project in Andalussia
 url <- 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
 
 # read url and convert to data.frame
+
+
 con = file(url, "r")
 data_code <- readLines(con)
-aws_data <- fromJSON(txt=data_code[6],force=TRUE)
-close(con)
+data_code_cleaned <- data_code[6]
+json <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)', "", data_code_cleaned, perl = TRUE)
+data <- fromJSON(json)
