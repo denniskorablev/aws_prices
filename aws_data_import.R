36c51f4762cb0bf26f504a0896cc6741d91a78cf
Dennis Lyubyvy <dennis.lyubyvy@gmail.com>
2015-03-18 12:19:13 -0400

    :sunny:

--- aws_data_import.R
+++ aws_data_import.R
@@ -8,14 +8,15 @@ if (!'RJSONIO' %in% installed.packages()) install.packages('RJSONIO')
 library('RJSONIO') 
 
 # url with some information about project in Andalussia
-url <- 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
+#url <- 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
+url <- 'linux-od.min.js'
 
 # read url and convert to data.frame
 data_code <- readLines(url)
 data_code_cleaned <- data_code[6]
-data_code_cleaned <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)',"", data_code_cleaned, perl = TRUE)
-data_code_cleaned <- gsub('({)(\\w+)(:)','{\"\\2\":', data_code_cleaned, perl = TRUE)
-data_code_cleaned <- gsub('(,)(\\w+)(:)',',\"\\2\":', data_code_cleaned, perl = TRUE)
+data_code_cleaned <- gsub('([a-zA-Z_0-9\\.]*\\()|(\\);?$)',"", data_code_cleaned, perl = TRUE) #eliminate function call
+data_code_cleaned <- gsub('({)(\\w+)(:)','{\"\\2\":', data_code_cleaned, perl = TRUE) #add quotes
+data_code_cleaned <- gsub('(,)(\\w+)(:)',',\"\\2\":', data_code_cleaned, perl = TRUE) #add quotes
 data <- (fromJSON(data_code_cleaned))
 
 #extract AWS information from the JSON
@@ -31,8 +32,8 @@ for (i in 1:length(data$config$regions)) {
 aws_prices <- {} 
 for (i in 1:length(aws_regions)) {
     aws_regions_data <- data$config$regions[[i]]$instanceTypes #extract data for specific region
-    aws_sizes <- aws_regions_data[[i]]$sizes # extract data for different server sizes 
-    n_sizes <- length(aws_sizes) #store number of server sizes
+    #aws_sizes <- aws_regions_data[[i]]$sizes # extract data for different server sizes 
+    #n_sizes <- length(aws_sizes) #store number of server sizes
     
     # extract server types
     aws_types <- {} 
@@ -48,7 +49,7 @@ for (i in 1:length(aws_regions)) {
         aws_sizes <- aws_regions_data[[j]]$sizes
         aws_info_for_region_and_size <- {}
         for (k in 1:length(aws_sizes)) { #loop for server sizes
-            aws_info_for_region_and_size <- rbind(aws_info_for_region_and_size,unlist(aws_regions_data[[j]]$sizes[k]))
+            aws_info_for_region_and_size <- rbind(aws_info_for_region_and_size,unlist(aws_regions_data[[j]]$sizes[k])[1:7])
         }
         temp_aws <- cbind(type=aws_types[j],aws_info_for_region_and_size)
         aws_info <- rbind(aws_info,temp_aws)
@@ -57,9 +58,10 @@ for (i in 1:length(aws_regions)) {
     aws_prices <- rbind(aws_prices,aws_info_full)
 }
 colnames(aws_prices)[8:9] <- c('OS','USD')
+aws_prices_df <- as.data.frame(aws_prices)
+na.omit(aws_prices_df)
 
 #our final data object
-head(aws_prices)
-
+head(aws_prices_df)
 
 
